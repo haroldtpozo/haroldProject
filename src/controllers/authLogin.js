a2baed882c34ac14b657d848153cb14bad125318
Harold <haroldtpozo@gmail.com>
2023-12-28 15:09:09 +0100

    Segundo commit: bcrypt

--- src/controllers/authLogin.js
+++ src/controllers/authLogin.js
@@ -1,20 +1,28 @@
 import User from "../mongo/schemas/user.model.js"
+import bcrypt from "bcrypt"
 
 
 export const login = async (req,res) =>{
-    const {email,password} = req.body;
+    
     try {
+        const {email,password} = req.body;
+        if (email === "" || password === "") {
+            return res.status(400).send("Colocar...")
+        }
         const userFound = await User.findOne({email})
         if (!userFound) {
             return res.status(400).json({message:"User not found"});}
 
-        if (password === userFound.password){
+        const valPassword = bcrypt.compareSync(password, userFound.password);
+    
+        if (valPassword){
             return res.status(200).json({message:"Login sucessful"}) 
         } else{
             return res.status(400).json({message:"Incorrect password"})
         }
 
     } 
+    
     catch (error) {
         console.error(error)
         return res.status(500).json({message:"Internal Server Error"})
--- src/controllers/authRegister.js
+++ src/controllers/authRegister.js
@@ -1,10 +1,29 @@
 import User from "../mongo/schemas/user.model.js"
-
+import bcrypt from "bcrypt";
 
 export const register = async (req,res) =>{
-    const {email,password,username} = req.body;
-
     try {
+    const {email,password,username} = req.body;
+    if (email === "" || password === "" || username === "" ) {
+        return res.status(400).send("Colocar...")
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    const emailVal=emailRegex.test(email)
+    if (!emailVal) {
+        return res.status(400).send("Email no valido")
+    }
+    const emailFound= await User.findOne({email})
+    if (emailFound) {
+        return res.status(400).send("Email ya en uso");
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const encryptedPass = bcrypt.hashSync(password,salt)
+
+    console.log(salt,encryptedPass)
+    //const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
         const newUser = new User({
         username,
         email,
@@ -16,7 +35,7 @@ export const register = async (req,res) =>{
     console.log(email,password,username);
     
     } catch (error) {
-        console.log(error);
+        return res.status(400).send("Ha ocurrido un error inesperado");
     }
 
 
